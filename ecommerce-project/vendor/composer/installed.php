d867c598f3ff39fbfbc0d9d67b0aa3ac98a47639
Wrehaspati <gedereyna@gmail.com>
2023-02-19 20:18:18 +0800

    Installing Starter Kits
    Breeze & Blaze:
    
    # require npm
    - please install npm through Google until "npm run dev" command.
    
    # require MySQLi database
    start mysqli engine with XAMPP, then:
    - php artisan migrate
    
    * navigate to your application's with /login or /register URLs in your web browser. all of breeze's routes are defined within the routes/auth.php file.

--- ecommerce-project/vendor/composer/installed.php
+++ ecommerce-project/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v10.0.2',
-        'version' => '10.0.2.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0b465c830127b91d9aad2b168e33ed0cb7a09cbc',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -361,6 +361,15 @@
                 0 => '*',
             ),
         ),
+        'laravel/breeze' => array(
+            'pretty_version' => 'v1.19.1',
+            'version' => '1.19.1.0',
+            'reference' => '4bbb1ea3476901c4f5fc706f8d80e4eac31c3afb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laravel/breeze',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
         'laravel/framework' => array(
             'pretty_version' => 'v10.0.3',
             'version' => '10.0.3.0',
@@ -371,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v10.0.2',
-            'version' => '10.0.2.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0b465c830127b91d9aad2b168e33ed0cb7a09cbc',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -715,8 +724,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
