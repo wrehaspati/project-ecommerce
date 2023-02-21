96ea54914d87904c31c7d7cda9652d1c96958055
Wrehaspati <gedereyna@gmail.com>
2023-02-21 15:42:21 +0800

    Update and Restore

---
+++ ecommerce/database/migrations/2023_02_20_222629_create_item_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->nullable()->constrained()->nullOnDelete()->cascadeOnUpdate();
+            $table->string('variant_name');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->text('description');
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_details');
+    }
+};
