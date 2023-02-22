8a262e5ea97377ebd385242a57226d12af4749f7
Wrehaspati <gedereyna@gmail.com>
2023-02-22 23:54:26 +0800

    Create ItemDetailFactory.php

---
+++ ecommerce/database/factories/ItemDetailFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ItemDetail>
+ */
+class ItemDetailFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'item_id' => fake()->numberBetween(1, 10),
+            'variant_name' => fake()->word(),
+            'quantity' => fake()->numberBetween(1, 15),
+            'price' => fake()->numberBetween(500000, 5000000),
+            'description' => fake()->text(),
+            'image' => 'https://via.placeholder.com/300.png'
+        ];
+    }
+}
