90a2160b6f7b2cd3eae82cd796b85b7ff503de95
Wrehaspati <gedereyna@gmail.com>
2023-03-04 20:45:51 +0800

    Update Footer

--- ecommerce/resources/views/layouts/app.blade.php
+++ ecommerce/resources/views/layouts/app.blade.php
@@ -36,14 +36,14 @@
                     </div>
                 </header>
             @endif    
-            @if(isset($main))
+
             <main class="bg-white shadow">
-                {{ $main }}
-                {{-- {{ $slot }} --}}
+                {{ $slot }}
             </main>
-            @endif
         </div>
+        
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <script src="{{ url(assert('js/homepage.js')) }}"></script>
     </body>
+        @include('layouts.footer')
 </html>
