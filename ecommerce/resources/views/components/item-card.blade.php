9045f3f9321e74a149b790ed75bc6d89635a5838
Alcadeius <andresentanu3@gmail.com>
2023-03-05 21:40:19 +0800

    Update Card Item
    Major Improvement on UI more flexibel and responsive

--- ecommerce/resources/views/components/item-card.blade.php
+++ ecommerce/resources/views/components/item-card.blade.php
@@ -1,62 +1,112 @@
-<div class="swiper">
-    <!-- Additional required wrapper -->
-    <div class="swiper-wrapper text-center mb-5">
-        <!-- Slides -->
-        <div class="swiper-slide">
-            <div class="flex w-full ml-5">
-                <div class="cad group/first text-white">
-                    <img class="absolute z-10 top-16 left-4 flex hover:scale-125 transition-all duration-100"
-                        id="fist" width="40%" src="img/kalung.png" alt="">
-                    <img class="absolute z-10 opacity-1 flex top-16 left-4 hover:scale-125 transition-all duration-100"
-                        id="okeh" width="40%" src="img/kalung-item.png" alt="">
-                    <p class="flex absolute -right-9 font-serif uppercase font-bold top-5 text-xl z-10">Lucky
-                        Necklace</p>
-                    <p class="flex absolute font-mono right-16 text-lg font-bold top-11 z-10">$ 1.230.21</p>
-                    <p class="flex absolute z-10 left-1/2 bottom-1/2 font-serif text-xs uppercase">Variant</p>
-                    <div class="flex flex-row absolute z-10 w-3/5 left-1/2 bottom-1/3 text-xs">
-                        <div
-                            class="bg-slate-600 w-2/3 h-5 rounded-full flex hover:ring-gray-900 hover:ring-inset hover:ring-2 hover:shadow-xl hover:shadow-white/50 transition-all duration-300">
-                            <p class="bg-black w-3 h-3 mt-1 ml-1 rounded-full"></p>
-                            <a onclick="morph()" style="margin-top: 2px;margin-left: 4px;"
-                                class="cursor-pointer">Vanta Black</a>
-                        </div>
-                        <div
-                            class="bg-slate-600 ml-1 w-2/3 h-5 rounded-full flex hover:ring-red-500 hover:ring-inset hover:ring-2 hover:shadow-xl hover:shadow-red-600/40 transition-all duration-300">
-                            <p class="bg-red-900 w-3 h-3 mt-1 ml-1 rounded-full"></p>
-                            <a onclick="morphin()" style="margin-top: 2px;margin-left: 4px;"
-                                class="cursor-pointer">Red Blood</a>
-                        </div>
-                    </div>
-                    <div
-                        class="flex absolute z-10 left-1/2 bottom-4 group/second w-2/5 text-sm overflow-hidden">
-                        <p
-                            class="absolute left-1 z-10 flex group-hover/second:-translate-x-32 transition-all duration-700 bg-black -translate-x-80 rotate-45 w-96 h-96 ">
-                        </p>
-                        <div class="bg-red-800 w-full p-2">
-                            <a class="ml-5 font-bold after:absolute relative z-10 after:content-['-'] after:group-hover/second:bg-white after:group-hover/second:delay-700 after:group-hover/second:duration-400 after:group-hover/second:transition-all after:text-white after:h-2 after:bg-black after:-right-4 after:-bottom-3"
-                                href="">
-                                <img src="img/cart.svg" class="flex absolute -top-1 w-5" alt="">
-                                Add To Cart
-                            </a>
-                        </div>
-                    </div>
-                    <div class="flex absolute bottom-4 -right-10 z-10 text-sm">
-                        <a href="#"><img width="50%" height="50%" src="img/heart.svg"
-                                alt=""></a>
-                    </div>
-                    <div
-                        class="liner before:group-hover/first:opacity-100 before:opacity-1 before:rounded-xl transition-all duration-500 rounded-lg">
-                        a</div>
+{{-- <div class="flex flex-row">
+    <div class="flex ml-3">
+        <div class="cad group/first text-white">
+            <img class="absolute z-10 hidden top-16 left-4 hover:scale-110 transition-all duration-75" id="okeh" width="30%" src="img/kalung-item.png" alt="">
+            <img class="absolute z-10 top-16 left-4 flex hover:scale-110 transition-all duration-75" id="fist" width="30%" src="img/kalung.png" alt="">
+            <p class="flex absolute left-1/3 font-serif uppercase font-bold top-5 text-xl z-10">Lucky Necklace</p>
+            <p class="flex absolute font-mono left-1/3 text-lg font-bold top-11 z-10">$ 1.230.21</p>
+            <p class="flex absolute z-10 left-1/3 bottom-1/2 font-serif text-xs uppercase">Variant</p>
+            <div class="flex flex-row absolute z-10 w-3/5 left-1/3 bottom-1/3 text-xs">
+                <div class="bg-slate-600 w-1/3 h-5 rounded-full flex hover:ring-gray-900 hover:ring-inset hover:ring-2 hover:shadow-xl hover:shadow-white/50 transition-all duration-300">
+                    <p class="bg-black w-3 h-3 mt-1 ml-1 rounded-full"></p>
+                    <a onclick="morph()" style="margin-top: 2px;margin-left: 4px;" class="cursor-pointer">Vanta Black</a>
+                </div>
+                <div
+                    class="bg-slate-600 ml-1 w-1/3 h-5 rounded-full flex hover:ring-red-500 hover:ring-inset hover:ring-2 hover:shadow-xl hover:shadow-red-600/40 transition-all duration-300">
+                    <p class="bg-red-900 w-3 h-3 mt-1 ml-1 rounded-full"></p>
+                    <a onclick="morphin()" style="margin-top: 2px;margin-left: 4px;"
+                        class="cursor-pointer">Red Blood</a>
                 </div>
             </div>
+            <div class="flex absolute z-10 left-1/3 bottom-4 group/second w-fit text-sm overflow-hidden">
+                <p class="absolute flex group-hover/second:-translate-x-32 transition-all duration-700 bg-black -translate-x-80 rotate-45 w-96 h-96 "></p>
+                <div class="bg-red-800 p-2 overflow-hidden">
+                    <a class="ml-5 font-bold after:absolute relative z-10 after:content-['-'] after:group-hover/second:bg-white after:group-hover/second:delay-700 after:group-hover/second:duration-400 after:group-hover/second:transition-all after:text-white after:h-2 after:bg-black after:-right-[0.30rem] after:-bottom-3" href="">
+                        <img src="img/cart.svg" class="flex absolute -left-6 -top-1 w-5" alt="">Add To Cart
+                    </a>
+                </div>
+            </div>
+            <div class="flex absolute bottom-4 w-1/2 h-1/6 left-2/3 z-10 text-sm">
+                <img src="img/heart.svg" alt="">
+            </div>
+            <div class="liner before:group-hover/first:opacity-100 before:opacity-1 before:rounded-xl transition-all duration-500 rounded-lg"></div>
         </div>
     </div>
-    <!-- If we need pagination -->
-    <div class="swiper-pagination"></div>
+</div> --}}
 
-    <div class="pagination">
-        <!-- If we need navigation buttons -->
-        <div class="swiper-button-prev ml-10"><img src="img/prev.svg" alt=""></div>
-        <div class="swiper-button-next mr-10"><img src="img/next.svg" alt=""></div>
+{{-- <div class="flex flex-row">
+<div class="max-w-[25rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="rounded-t-lg relative z-1" width="100%" src="img/model.jpeg" alt=""/>
+        <img class="rounded-t-lg absolute" src="img/model2.jpeg" alt=""/>
+    </a>
+    <div class="p-5 mt-20">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </a>
+    </div>
+</div>
+</div> --}}
+<div class="container flex handphone:max-w-sm tablet:max-w-[35rem] laptop:max-w-[55rem] lg:max-w-[65rem] xl:max-w-[75rem]">
+    <div class="bg-slate-50 tablet:-mt-[0.5rem] w-1/2 h-3/4">
+        <a href="#" class="relative z-10 top-2 lg:top-0 lg:left-1 laptop:top-0 left-2"><img src="img/heart.svg" class="tablet:w-[15%] xl:w-[10%] laptop:w-[12%]" width="10%" alt=""></a>
+        <div class="relative tablet:-mt-[1.90rem] laptop:-mt-[2.3rem] handphone:-mt-7 -mt-5 z-1">
+            <img class="w-full" src="img/model.jpeg" alt="">
+        </div>
+        <div class="p-2">
+            <h1 class="mb-2">Lucky Necklace</h1>
+            <p class="text-sm mb-2">Menambah Stat keberuntungan sebesar 1</p>
+            <h2>$120.99</h2>
+        </div>
+        <div class="flex p-2  bg-blue-400">
+            <a class="flex handphone:ml-[3rem] tablet:ml-[2.5rem] laptop:ml-[3rem] lg:ml-[5rem] xl:ml-24 ml-12 text-white" href="#"><p class="text-sm">Add To Cart</p><img class="tablet:flex hidden" src="img/arrow.svg" width="13%" alt=""></a>
+        </div>
+    </div>
+    <div class="bg-slate-50 tablet:-mt-[0.5rem] ml-3 w-1/2 h-3/4">
+        <a href="#" class="relative z-10 top-2 lg:top-0 lg:left-1 laptop:top-0 left-2"><img class="tablet:w-[15%] xl:w-[10%] laptop:w-[12%]" src="img/heart.svg" width="10%" alt=""></a>
+        <div class="relative tablet:-mt-[1.90rem] laptop:-mt-[2.3rem] handphone:-mt-7 -mt-5 z-1">
+            <img class="w-full" src="img/model.jpeg" alt="">
+        </div>
+        <div class="p-2">
+            <h1 class="mb-2">Lucky Necklace</h1>
+            <p class="text-sm mb-2">Menambah Stat keberuntungan sebesar 1</p>
+            <h2>$120.99</h2>
+        </div>
+        <div class="flex p-2  bg-blue-400">
+            <a class="flex handphone:ml-[3rem] tablet:ml-[2.3rem] laptop:ml-[3rem] lg:ml-[5rem] xl:ml-24 ml-12 text-white" href="#"><p class="text-sm">Add To Cart</p><img class="tablet:flex hidden" src="img/arrow.svg" width="13%" alt=""></a>
+        </div>
+    </div>
+    <div class="hidden tablet:block tablet:-mt-[0.5rem] bg-slate-50 ml-3 w-1/2 h-3/4">
+        <a href="#" class="relative z-10 top-2 lg:top-0 lg:left-1 laptop:top-0 left-2"><img class="tablet:w-[15%] xl:w-[10%] laptop:w-[12%]" src="img/heart.svg" width="10%" alt=""></a>
+        <div class="relative tablet:-mt-[1.90rem] laptop:-mt-[2.3rem] -mt-4 z-1">
+            <img class="w-full" src="img/model.jpeg" alt="">
+        </div>
+        <div class="p-2">
+            <h1 class="mb-2">Lucky Necklace</h1>
+            <p class="text-sm mb-2">Menambah Stat keberuntungan sebesar 1</p>
+            <h2>$120.99</h2>
+        </div>
+        <div class="flex p-2  bg-blue-400">
+            <a class="flex handphone:ml-[4rem] tablet:ml-[2.3rem] laptop:ml-[3rem] lg:ml-[5rem] xl:ml-24 ml-12 text-white" href="#"><p class="text-sm">Add To Cart</p><img class="tablet:flex hidden" src="img/arrow.svg" width="13%" alt=""></a>
+        </div>
+    </div>
+    <div class="hidden laptop:block laptop:-mt-[0.5rem] bg-slate-50 ml-3 w-1/2 h-3/4">
+        <a href="#" class="relative z-10 top-2 lg:top-0 lg:left-1 laptop:top-0 left-2"><img class="tablet:w-[15%] xl:w-[10%] laptop:w-[12%]" src="img/heart.svg" width="10%" alt=""></a>
+        <div class="relative tablet:-mt-[1.90rem] laptop:-mt-[2.3rem] -mt-4 z-1">
+            <img class="w-full" src="img/model.jpeg" alt="">
+        </div>
+        <div class="p-2">
+            <h1 class="mb-2">Lucky Necklace</h1>
+            <p class="text-sm mb-2">Menambah Stat keberuntungan sebesar 1</p>
+            <h2>$120.99</h2>
+        </div>
+        <div class="flex p-2  bg-blue-400">
+            <a class="flex handphone:ml-[4rem] tablet:ml-[3.3rem] laptop:ml-[3rem] lg:ml-[5rem] xl:ml-24 ml-12 text-white" href="#"><p class="text-sm">Add To Cart</p><img class="tablet:flex hidden" src="img/arrow.svg" width="13%" alt=""></a>
+        </div>
     </div>
 </div>
