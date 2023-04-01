eb72c661c3c03016c35c32f18e275714330402d3
Wrehaspati <gedereyna@gmail.com>
2023-04-01 13:20:14 +0800

    Auth Remake

--- ecommerce/resources/views/auth/login.blade.php
+++ ecommerce/resources/views/auth/login.blade.php
@@ -3,61 +3,73 @@
 
     {{-- Header --}}
     <x-slot name="header">
-        <h2 class="text-center text-4xl text-indigo-900 font-display font-semibold lg:text-left xl:text-5xl
-        xl:text-bold">
-        Log In</h2>
+        Welcome Back!
     </x-slot>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
+    
     <form method="POST" action="{{ route('login') }}">
         @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
+        <a href="#"
+            class="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100">
+            <div class="px-4 py-3">
+                <svg class="h-6 w-6" viewBox="0 0 40 40">
+                    <path
+                        d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                        fill="#FFC107" />
+                    <path
+                        d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
+                        fill="#FF3D00" />
+                    <path
+                        d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
+                        fill="#4CAF50" />
+                    <path
+                        d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                        fill="#1976D2" />
+                </svg>
+            </div>
+            <h1 class="px-4 py-3 w-5/6 text-center text-gray-600 font-bold">Sign in with Google</h1>
+        </a>
+        <div class="mt-4 flex items-center justify-between">
+            <span class="border-b w-1/5 lg:w-1/4"></span>
+            <a href="#" class="text-xs text-center text-gray-500 uppercase">or login with email</a>
+            <span class="border-b w-1/5 lg:w-1/4"></span>
+        </div>
+        <div class="mt-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-
-        <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')"/>
+            <div class="flex justify-between">
+                <label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                @if (Route::has('password.request'))
+                    <a class="text-xs text-gray-500 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+            </div>
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
-
-        <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-        </div>
-        
-        <div class="mt-6">
+        <div class="mt-8">
             <button type="submit"
-                class="bg-indigo-500 text-gray-100 p-4 w-full rounded-full tracking-wide
-                font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
-                shadow-lg">
-                Log In
-            </button>
+                class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Login</button>
         </div>
-        <div class="mt-6 text-sm font-display font-semibold text-gray-700 text-center">
-            Don't have an account ? <a href="{{ route('register') }}" class="cursor-pointer text-indigo-600 hover:text-indigo-800">Sign
-                up</a>
+        <div class="mt-4 flex items-center justify-between">
+            <span class="border-b w-1/5 md:w-1/4"></span>
+            <a href="{{ route('register') }}" class="text-xs text-gray-500 uppercase">or sign up</a>
+            <span class="border-b w-1/5 md:w-1/4"></span>
         </div>
     </form>
 </x-guest-layout>
